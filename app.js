a2ff04a19702ee420e7f7e3a9500a57b9352d321
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-06 19:06:14 -0500

    added delete prompts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { mainMenu, searchEmployee } = require('./utils/prompts.js');
 const { addDepartment, addRole, addEmployee } = require('./utils/addprompts');
 const { updateRole, updateManager } = require('./utils/updateprompts');
+const { deleteDepartment, deleteRole, deleteEmployee } = require('./utils/deleteprompts');
 
 const handleSelection = function (menu) {
     console.log(menu.selection);
