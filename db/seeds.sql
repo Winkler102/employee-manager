b4ed1e5daada532198590eeba3e19e980c9b3d30
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-09 12:41:28 -0500

    Updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,17 @@
 INSERT INTO department (name)
 VALUES
     ('marketing'),
-    ('R&D'),
-    ('DeleteME');
+    ('R&D');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
     ('Manager', 1.01, 1),
-    ('Developer', 10.01, 2),
-    ('delete', 0.00, 3);
+    ('Developer', 10.01, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
     ('John', 'Doe', 1, NULL),
     ('Lawn', 'Joe', 2, 1),
-    ('Mawn', 'Moe', 1, 1),
-    ('Delete', 'Fool', 3, 1);
+    ('Mawn', 'Moe', 1, 1);
+
+UPDATE employee SET manager_id =1 WHERE id=1;
