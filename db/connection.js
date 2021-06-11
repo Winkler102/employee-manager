b94a2e2f2cc84538569c8d6ed104608d8b8fb295
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-11 13:08:21 -0500

    added .env file

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'open',
-    database: 'employeesManage'
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
 });
 
 module.exports = db;
